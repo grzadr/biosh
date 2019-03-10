f427c33a5514eca331d684515b1ccd8cb59113d3
Adrian Grzemski <adrian.grzemski@gmail.com>
2019-03-10 17:00:52 +0100

    Rewritten MAFtoTSV script

--- MAFtoTSV.sh
+++ MAFtoTSV.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 mawk '
+function join(array, sep, start, end, result, i)
+{
+  if (sep == "") sep = " "
+
+  if (start = "") start = 1
+  result = array[start]
+
+  if (end = "") end = length
+
+  for (i = start + 1; i <= end; ++i)
+    result = result sep array[i]
+  return result
+}
 BEGIN {RS=""; FS="\n"}
 {
   if ($1 !~ /^a/) {
@@ -9,108 +22,82 @@ BEGIN {RS=""; FS="\n"}
     exit 1
   }
 
-  ref_n=split($(NF-1), ref, " ")
+  result_n = 0
+  split("", result, ":") # init an empty array "result"
+  split("", result_reqs, ":") # init an empty array "result_seqs"
 
-  if (ref_n != 7) {
-    print "Number of fields is different from 7"
-    print $0
+  for (i = 1; i <= NF; ++i){
+    if ($i !~ /^s\ /) {
+      continue
+    }
+
+    ++result_n;
+
+    record_n = split($i, record, " ")
+
+    if (record_n != 7) {
+      print "Number of fields " record_n "is different from 7" > "/dev/stderr"
+      print $i > "/dev/stderr"
+      print "\nFrom record\n" > "/dev/stderr"
+      print $0 > "/dev/stderr"
+      exit 1
+    }
+
+    species = substr(record[2], 1, index(record[2], ".") - 1)
+    chrom = substr(record[2], index(record[2], ".") + 1)
+
+    first = record[3]
+    match_len = record[4]
+    strand = record[5]
+    chrom_len = record[6]
+
+    if (strand == "+") {
+      start = first + 1
+      end = start + match_len
+    } else if (strand == "-") {
+      end = chrom_len - start
+      start = end - match_len + 1
+    } else {
+      print "Unknown strand format" > "/dev/stderr"
+      print $0 > "/dev/stderr"
+      exit 1
+    }
+
+    result[result_n] = species "\t" chrom "\t" start "\t" end "\t" strand "\t" match_len
+    result_seqs[result_n] = tolower(record[7])
+
+  }
+
+  if (result_n != 2){
+    print "More than two records" > "/dev/stderr"
+    print $0 > "/dev/stderr"
     exit 1
   }
 
-  if (ref[1] != "s") {
-    print "Not s block"
-    print $0
-    print $(NF-1)
+  ref_seq_split_n = split(result_seqs[1], ref_seq_split, "")
+  query_seq_split_n = split(result_seqs[2], query_seq_split, "")
+
+  if (ref_seq_split_n != query_seq_split_n){
+    print "Sequences have different lengths" > "/dev/stderr"
+    print ref_seq_split_n " != " query_seq_split_n > "/dev/stderr"
+    print $0 > "/dev/stderr"
     exit 1
   }
-
-  split(ref[2], ref_name, ".")
-
-  ref_species = ref_name[1]
-  ref_chr = ref_name[2]
-
-  ref_first = ref[3]
-  ref_length = ref[4]
-  ref_strand = ref[5]
-  ref_chr_len = ref[6]
-
-  if (ref_strand == "+") {
-    ref_start = ref_first + 1
-    ref_end = ref_start + ref_length
-  } else if (ref_strand == "-") {
-    ref_end = ref_chr_len - ref_start
-    ref_start = ref_end - ref_length + 1
-  } else {
-    print $0
-    exit 1
-  }
-
-  query_n=split($NF, query, " ")
-
-  if (query_n != 7) {
-    print "Number of fields is different from 7"
-    print $0
-    exit 1
-  }
-
-  if (query[1] != "s") {
-    print "Not s block"
-    print $NF
-    exit 1
-  }
-
-  split(query[2], query_name, ".")
-
-  query_species = query_name[1]
-  query_chr = query_name[2]
-
-  query_first = query[3]
-  query_length = query[4]
-  query_strand = query[5]
-  query_chr_len = query[6]
-
-  if (query_strand == "+") {
-    query_start = query_first + 1
-    query_end = query_start + query_length
-  } else if (query_strand == "-") {
-    query_end = query_chr_len - query_first
-    query_start = query_end - query_length + 1
-  } else {
-    print $0
-    exit 1
-  }
-
-  split(tolower(ref[7]), ref_seq_split, "")
-  split(tolower(query[7]), query_seq_split, "")
 
   ref_seq=""
   query_seq=""
 
-    for (i=1; i in ref_seq_split; ++i){
-      if (ref_seq_split[i] == query_seq_split[i]) {
-        ref_seq = ref_seq "1"
-        query_seq = query_seq "1"
-      } else {
-        ref_seq = ref_seq (ref_seq_split[i] == "-" ? "" : "0")
-        query_seq = query_seq (query_seq_split[i] == "-" ? "" : "0")
-      }
+  for (i=1; i in ref_seq_split; ++i){
+    if (ref_seq_split[i] == query_seq_split[i]) {
+      ref_seq = ref_seq "1"
+      query_seq = query_seq "1"
+    } else {
+      ref_seq = ref_seq (ref_seq_split[i] == "-" ? "" : "0")
+      query_seq = query_seq (query_seq_split[i] == "-" ? "" : "0")
+    }
   }
 
-  print \
-    ref_species "\t" \
-    ref_chr "\t" \
-    ref_start "\t" \
-    ref_end "\t" \
-    ref_strand "\t" \
-    ref_length "\t" \
-    ref_seq "\t" \
-    query_species "\t" \
-    query_chr "\t" \
-    query_start "\t" \
-    query_end "\t" \
-    query_strand "\t" \
-    query_length "\t" \
-    query_seq
+  print result[1] "\t" ref_seq "\t" result[2] "\t" query_seq
 
 }
 '
