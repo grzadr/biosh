6f00f0b093b0a06e3cd92f5dc8207e2ffbadd696
Adrian Grzemski <adrian.grzemski@gmail.com>
2019-04-15 00:02:15 +0200

    Initial changes to tsv_pivot; think abou C++ implement

--- tsv_pivot.sh
+++ tsv_pivot.sh
@@ -6,12 +6,14 @@ BEGIN {
   OFS="\t"
   last_id = 0
   id_counter = 0
+  name_counter=0
   last_line = ""
+  split("", names)
 
 }
 NR == 1 {
   if (NF < 3){
-    print "Not enough columns"
+    print "Not enough columns" > "/dev/stderr"
     exit 1
   }
 
@@ -30,13 +32,42 @@ NR == 1 {
 
   next
 }
+function init_record(){
+  name_counter=1
+  last_id = $id_column
+  last_line = $1
+  for (i=2; i <= shared_columns; ++i)
+    last_line = last_line OFS $i
+
+  last_line = last_line OFS $value_column
+}
+NR == 2 {
+
+}
+function verify_name(expected, detected){
+  if (expected != detected){
+    print "Expected \"" expected \
+          "\" detected \"" detected \
+          "\" on line " NR > "/dev/stderr"
+    exit 1
+  }
+}
 {
   if ($id_column != last_id) {
     if (id_counter == 1) print header
-    if (id_counter > 0) print last_line
+    if (name_counter != length(names)){
+      print "Expected " length(names) \
+            " samples, but detected " \
+            name_counter > "/dev/stderr"
+    }
+    print last_line
     id_counter++
     last_id = $id_column
     last_line = $1
+
+    name_counter=1
+    verify_name(names[name_counter],
+                $name_column)
 
     for (i=2; i <= shared_columns; ++i)
       last_line = last_line OFS $i
@@ -44,10 +75,13 @@ NR == 1 {
     last_line = last_line OFS $value_column
 
   } else {
+    verify_name(names[++name_counter],
+                $name_column)
     last_line = last_line OFS $value_column
   }
 
   if (id_counter == 1){
+    names[++name_counter] = $name_column
     header = header OFS $name_column
   }
 }
