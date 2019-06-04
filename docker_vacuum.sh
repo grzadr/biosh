170288732e39cff477b91a929e2ee4ca7bb5d707
Adrian Grzemski <adrian.grzemski@gmail.com>
2019-06-04 13:22:01 +0200

    removed prompt for docker_vacuum.sh

--- docker_vacuum.sh
+++ docker_vacuum.sh
@@ -3,7 +3,7 @@
 # Basic commands to clear exited containers and clear dangling images
 
 # Removes exited containers
-docker container prune
+docker container prune -f
 
 # Removes dangling images
 docker images -qf dangling=true | xargs --no-run-if-empty docker rmi
